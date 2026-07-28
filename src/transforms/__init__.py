2f08e1db574448dd287e4f3c99e6e07e7d2df282
SeM0nchik <semone.pavlychev@gmail.com>
2026-07-28 17:13:51 +0300

    update: delete unused files from template

--- src/transforms/__init__.py
+++ src/transforms/__init__.py
@@ -1,4 +1,2 @@
-from src.transforms.normalize import Normalize1D
 from src.transforms.pad import ReplicationPad
-from src.transforms.scale import RandomScale1D
 from src.transforms.spectrogram import LogPowerSpectrogram
